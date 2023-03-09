f240a429ce4d17c464180bcb1ac6500e3e4bcad1
KashshafLabib <kashshaflabib@iut-dhaka.edu>
2023-03-10 00:32:58 +0600

    New Problem solved

---
+++ 318_Cake_assembly-line.cpp
@@ -0,0 +1,37 @@
+/*“So, be patient. Surely Allah’s promise is true, and let not the disbelievers shake your firmness” (Quran, 30:60)*/
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, w, h;
+        cin >> n >> h >> w;
+        vector<ll> a(n), b(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+            b[i] = a[i] - b[i];
+        }
+        sort(b.begin(), b.end());
+        if (b[n - 1] - b[0] <= 2 * (h - w))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
